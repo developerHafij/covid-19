060e4f2fc646a268867e473824c2e46e642f2a74
Hafijur Rahman <developer.hafij@gmail.com>
2020-05-04 15:59:49 +0600

    Upload the website

---
+++ index.php
@@ -0,0 +1,185 @@
+<!-- Include the Header File -->
+<?php include "header.php" ?>
+
+<!-- Include the Menubar FIle -->
+<?php include "menubar.php" ?>
+
+
+<!-- Start Banner Section -->
+<section class="banner-section" style="padding-top: 60px;">
+	<div class="container">
+		<div class="row full-banner cmn-padding">
+			<!-- Left Section Start -->
+			<div class="col-md-6 md-6 banner-details">
+				<span class="badge badge-danger covid-19">Covid-19</span>
+				<span class="c_arrow"></span>
+				<span class="virus"><strong>CoronaVirus</strong></span>
+				<h2>What is Coronavirus??</h2>
+				<p><span class="subtitle">Coronavirus (COVID-19) is an infectious disease caused by a newly discovered coronavirus.</span>
+					Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment. Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.
+				</p>
+				<div class="banner-button">
+					<a href=""><span class="btn btn-primary banner-b1">More About Covid-19</span></a>
+					<a href="https://www.who.int/"><span class="btn btn-primary banner-b2"><i class="fa fa-arrow-right"></i> Go WHO Website</span></a>
+				</div>
+			</div>
+			<!-- Left Section End -->
+
+			<!-- Right Section Start -->
+			<div class="col-md-6 md-6 banner-image">
+				<img src="images/bann.png">
+				<p>Keep Maintaining at least <b>3feet</b>(1meter)</p>
+			</div>
+			<!-- Right Section End -->
+		</div>
+	</div>
+</section>
+<!-- End Banner Section -->
+
+
+<!-- Start More About Section -->
+<section class="more-section">
+	<div class="border-top"></div>
+	<div class="container">
+		<div class="more-details cmn-padding">
+			<div class="row">
+				<div class="col-md-6">
+					<img src="images/more.png" width="65%" style="display: block; margin: 0 auto;">
+				</div>
+				<div class="col-md-6">
+					<div class="more-title">
+						<h2>More About Covid-19</h2>
+						<h6>Coronaviruses are a group of related RNA viruses that cause diseases in mammals and birds.</h6>
+						<p class="first">Coronaviruses are a large family of viruses that are known to cause illness ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS). Coronaviruses can cause colds with major symptoms, such as fever, and a sore throat from swollen adenoids. <br> <p class="last">Coronaviruses vary significantly in risk factor. Some can kill more than 30% of those infected, such as MERS-CoV, and some are relatively harmless, such as the common cold. Six species of human coronaviruses are known, with one species subdivided into two different strains, making seven strains of human coronaviruses altogether.</p></p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+<!-- End More About Section -->
+
+
+<!-- Start Symptoms Section -->
+<section class="symptoms-section">
+	<div class="divider-top"></div>
+	<div class="container">
+		<div class="symptoms cmn-padding">
+			<div class="symptoms-title">
+				<h2>Corona Virus Symptoms</h2>
+				<p>Coronavirus infects the lungs. The two main symptoms are a fever or a dry cough, which can sometimes lead to breathing problems</p>
+			</div>
+			<div class="row full-symp">
+				<!-- Left Symptomps -->
+				<div class="col-md-3">
+					<div class="row">
+						<div class="col-md-12 symp fever" id="Symp">
+							<p>You have a fever if your temperature is above 37.8C. This can make you feel warm.</p>
+						</div>
+						<div class="col-md-12 symp muscle">
+							<p>The cough to look out for is a new, continuous cough. This means coughing a lot for more than an hour, or having three or more coughing episodes in 24 hours. If you usually have a cough, it may be worse than usual.</p>
+						</div>
+					</div>
+				</div>
+				<!-- Middle Symptomps -->
+				<div class="col-md-6">
+					<img src="images/symptomps.png" width="100%">
+				</div>
+				<!-- Right Symptomps -->
+				<div class="col-md-3">
+					<div class="row">
+						<div class="col-md-12 symp headacke">
+							<p>Headache and diarrhoea have also been reported and a loss of smell and taste may also be a symptom.</p>
+						</div>
+						<div class="col-md-12 symp cough">
+							<p>Coughing a lot for more than an hour. If you usually have a cough, it may be worse than usual.</p>
+						</div>
+						<div class="col-md-12 symp dyspnoea">
+							<p>If you usually have a Dyspnoea, it may be worse than usual.</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<div class="divider-bottom"></div>
+</section>
+<!-- End Symptoms Section -->
+
+
+<!-- Start Prevention Section -->
+<section class="prevention-section">
+	<div class="container">
+		<div class="row cmn-padding">
+			<div class="col-md-6">
+				<div class="symptomps-title">
+					<h2>Prevention & Suggestion</h2>
+					<p>There is no vaccine for COVID-19. <b>So, The best thing is regular and thorough hand washing, preferably with soap and water.</b></p>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-2 image">
+									<img src="images/mask.png">
+								</div>
+								<div class="col-md-10 contents">
+									<h6>Use Mask</h6>
+									<p>coughing and sneezing into tissues, not touching your face with unwashed hands, and avoiding close contact.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-2 image">
+									<img src="images/wash.png">
+								</div>
+								<div class="col-md-10 contents">
+									<h6>Wash Hand</h6>
+									<p>The best thing is regular and thorough hand washing, preferably with soap and water.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-2 image">
+									<img src="images/avoid.png">
+								</div>
+								<div class="col-md-10 contents">
+									<h6>Avoid Hand Sheking</h6>
+									<p>Coronavirus infects the lungs. The two main symptoms are a fever or a dry cough, which can sometimes lead to breathing.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-2 image">
+									<img src="images/face.png">
+								</div>
+								<div class="col-md-10 contents">
+									<h6>Don't Touch Face</h6>
+									<p>People will be most infectious when they have symptoms, but some may spread the virus even before they are sick.</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<img src="images/doctor.png" width="100%">
+			</div>
+
+		</div>
+	</div>
+</section>
+<!-- Start Prevention Section -->
+
+
+<!-- Inlcude the Footer File -->
+<?php include "footer.php" ?>
+
+
+
+
+    
---
+++ menubar.php
@@ -0,0 +1,43 @@
+<section class="menubar-section">
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">COVID-19</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav ml-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="index.php" id="Home">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#" id="Symptomps">Symptomps</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#" id="Spread">Spread</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#" id="Prevention">Prevention</a>
+		      </li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
+		          Pages
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">About page</a>
+		          <a class="dropdown-item" href="#">BLog page</a>
+		          <a class="dropdown-item" href="#">Faq page</a>
+		          <a class="dropdown-item" href="#">Contact</a>
+		          <a class="dropdown-item" href="#">404 page</a>
+		          <!-- <div class="dropdown-divider"></div> -->
+		        </div>
+		      </li>
+		      <li class="">
+		        <span class="btn btn-primary">Help Line</span>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
+	</div>
+</section>
