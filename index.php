76bcda2b3d37eb5165f7fc86312bdddd497a982e
Hafijur Rahman <developer.hafij@gmail.com>
2020-05-17 17:00:31 +0600

    contact section

--- index.php
+++ index.php
@@ -402,6 +402,96 @@
 <!-- End Popular Blog -->
 
 
+<!-- Start Contact Section -->
+<section class="contact-section">
+	<div class="container">
+		<div class="row cmn-padding full-contact">
+			<div class="col-md-6 contact-title">
+				<h2>Contact in <span style="color: #ff7220;font-size: 27px;font-weight: 700;">Emergency</span> Situation</h2>
+				<span>Virus spreads when an infected person coughs or sneezes small droplets. These can be breathed in, or cause an infection.When an infected person coughs or sneezes small droplets. These can be breathed in, or cause an infection.</span>
+				<div class="emergency">
+					<p>Emergency Contacts: </p> 
+					<div class="row contact">
+						<div class="col-md-2 cmn-contact left-contact">
+							<ul>
+								<li>Telephone </li>
+								<li>Mobile </li>
+								<li>Phone </li>
+								<li>Email </li>
+								<li>Fax </li>
+							</ul>
+						</div>
+						<div class="col-md-10 cmn-contact right-contact">
+							<ul>
+								<li>: +565 65132 </li>
+								<li>: +880 1712345678 </li>
+								<li>: +880 1812345678 </li>
+								<li>: covid19@gmail.com </li>
+								<li>: +1323 555 1234 </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<span>These can be breathed in, or cause an infection. For more Latest information about Covid-19 please, visit <a href="https://www.who.int/">who website</a> & <a href="https://corona.gov.bd/">corona.gov.bd</a></span>
+			</div>
+			<!-- Form Section Start -->
+			<div class="col-md-6 form-right">
+				<form>
+					<h3>Send Message</h3>
+					<div class="form-row">
+						<div class="form-group col-md-12">
+						    <input type="text" class="form-control" id="name" placeholder="Full Name">
+						</div>
+					    <div class="form-group col-md-6">
+					      <input type="email" class="form-control" id="email" placeholder="Email">
+					    </div>
+					    <div class="form-group col-md-6">
+					      <input type="phone" class="form-control" id="" placeholder="Phone">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <input type="text" class="form-control" id="inputAddress" placeholder="Your Address">
+					  </div>
+					  <div class="form-row">
+					    <div class="form-group col-md-4">
+					      <select id="inputState" class="form-control">
+					        <option selected>Your Age</option>
+					        <option>00-05</option>
+					        <option>05-10</option>
+					        <option>10-15</option>
+					        <option>15-20</option>
+					        <option>20-25</option>
+					        <option>25-30</option>
+					        <option>30-35</option>
+					      </select>
+					    </div>
+					    <div class="form-group col-md-4">
+					      <select id="inputState" class="form-control">
+					        <option selected>Choose State</option>
+					        <option>Dhaka</option>
+					        <option>Rangpur</option>
+					        <option>Gazipur</option>
+					        <option>Rajshahi</option>
+					        <option>Barishal</option>
+					      </select>
+					    </div>
+					    <div class="form-group col-md-4">
+					      <input type="text" class="form-control" id="inputZip" placeholder="Zip Code">
+					    </div>
+					    <div class="form-group col-md-12">
+						    <textarea type="text" class="form-control" id="" placeholder="Your Message" rows="5"></textarea>
+						</div>
+					  </div>
+					  <button type="submit" class="btn btn-primary">Submit <i class="fa fa-paper-plane"></i></button>
+				</form>
+			</div>
+			<!-- Form Section End -->
+		</div>
+	</div>
+</section>
+<!-- End Contact Section -->
+
+
 <!-- Inlcude the Footer File -->
 <?php include "footer.php" ?>
 
